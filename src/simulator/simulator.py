d82424200814e417f0559df42e2e3d354b41bea6
Bigsmooth68 <olivier.spiesser@gmail.com>
2024-06-22 16:58:50 +0200

    rearrange simulator + code comment

--- src/simulator/simulator.py
+++ src/simulator/simulator.py
@@ -1,14 +1,13 @@
-from kafka import KafkaProducer
-import os, sys, json, time
-import argparse
-
 # export PYTHONPATH=.
+import sys, os
 sys.path.insert(0, os.getcwd())
+
+from kafka import KafkaProducer
+import json, time, argparse
+from src.reader.reader import read_messages_files
 
 def current_milli_time():
     return round(time.time() * 1000)
-
-from src.reader.reader import read_messages_files
 
 if __name__ == '__main__':
 
@@ -26,13 +25,16 @@ if __name__ == '__main__':
     # Play messages
     previous_time = 0
     for m in messages:
+        # Get message timestamp and update it with current time
         m_time = m['timestamp']
+        m['timestamp'] = current_milli_time()
 
+        # Determine how much time we should wait between messages
         sleep_time = m_time - previous_time
         print(f'{current_milli_time()} - Sleeping {sleep_time}s')
         time.sleep(sleep_time)
-        previous_time = m_time
+        previous_time = m_time # Move time forward based on current time
 
-        m['timestamp'] = current_milli_time()
+        # Produce message
         producer.send(topic='simulator', value=json.dumps(m).encode('UTF-8'))
         print(f'{current_milli_time()} - Message sent {m["MessageType"]}')
