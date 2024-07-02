c8a744106f4fcf49c17539ef5ffc4844c9a0ca93
Bigsmooth68 <olivier.spiesser@gmail.com>
2024-07-02 16:19:32 +0200

    logging added

--- src/simulator/simulator.py
+++ src/simulator/simulator.py
@@ -1,5 +1,5 @@
 # export PYTHONPATH=.
-import sys, os
+import sys, os, logging
 sys.path.insert(0, os.getcwd())
 
 from kafka import KafkaProducer
@@ -9,16 +9,30 @@ from src.reader.reader import read_messages_files
 def current_milli_time():
     return round(time.time() * 1000)
 
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(sys.stdout)
+fmt = '%(asctime)s - %(levelname)s - %(message)s'
+formatter = logging.Formatter(fmt)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--broker", type=str, help="URL to Kafka Broker. Default localhost:9092", default="localhost:9092")
-    parser.add_argument("--repeat", "-r", action='store_true')
+    parser.add_argument("--repeat", "-r", action='store_true', help="Loop on files")
+    parser.add_argument("--debug", "-d", action="store_true", help="Debug mode")
     parser.add_argument("PATH", type=str, help="Path of files to read and push to Kafka")
     args = parser.parse_args()
     path = args.PATH
     bs = args.broker
     repeat = args.repeat
+    debug = args.debug
+
+    if debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
 
     # Get list of messages
     messages = read_messages_files(path)
@@ -26,11 +40,10 @@ if __name__ == '__main__':
     # Get Kafka producer
     try:
         producer = KafkaProducer(bootstrap_servers=bs)
-        print(f"Connected to {bs}")
+        logger.info(f"Connected to {bs}")
     except:
-        print(f'Not able to connect')
+        logger.error(f'Not able to connect')
         exit(1)
-
     previous_time = 0    
     while True:
         # Play messages
@@ -45,12 +58,16 @@ if __name__ == '__main__':
             sleep_time = m_time - previous_time
             sleep_time = 0 if sleep_time < 0 else sleep_time # Prevent negativ value on loop
 
-            print(f'{current_milli_time()} - Sleeping {sleep_time}s')
+            logger.info(f'Sleeping {sleep_time}s')
             time.sleep(sleep_time)
             previous_time = m_time # Move time forward based on current time
 
             # Produce message
             producer.send(topic='simulator', value=json.dumps(local_messsage).encode('UTF-8'))
-            print(f'{current_milli_time()} - Message sent {m["MessageType"]}')
-        if not repeat:
+            logger.info(f'Message {m["MessageType"]} sent')
+            logger.debug(m)
+        if repeat:
+            logger.info('Repeating ...')
+        else:
+            logger.info('Completed')
             break
