1014e4e9e475d8d635d7efd6d6f02a330f44d0b7
Bigsmooth68 <olivier.spiesser@gmail.com>
2024-07-15 11:23:01 +0200

    add random option to simu

--- src/simulator/simulator.py
+++ src/simulator/simulator.py
@@ -6,6 +6,7 @@ from kafka import KafkaProducer
 import json, time, argparse
 from src.reader.reader import read_messages_files
 import src.custom_logger.custom_logger as custom_logger
+import random
 
 def current_milli_time():
     return round(time.time() * 1000)
@@ -17,6 +18,7 @@ if __name__ == '__main__':
     parser.add_argument("--repeat", "-r", action='store_true', help="Loop on files")
     parser.add_argument("--fast", "-f", action="store_true", help="2x speed")
     parser.add_argument("--FAST", "-F", action="store_true", help="4x speed")
+    parser.add_argument("--random", action="store_true", help="Replace OPC, DPC, CIC with random numbers and run at full speed.")
     parser.add_argument("--debug", "-d", action="store_true", help="Debug mode")
     parser.add_argument("PATH", type=str, help="Path of files to read and push to Kafka")
     args = parser.parse_args()
@@ -25,6 +27,7 @@ if __name__ == '__main__':
     repeat = args.repeat
     debug = args.debug
     fast = args.fast
+    random_option = args.random
 
     if debug:
         logger = custom_logger.custom_logger(__name__, debug)
@@ -52,14 +55,27 @@ if __name__ == '__main__':
     while True:
         # Play messages
 
+        if random_option:
+            # generate fake OPC, DPC, CIC
+            random_OPC = random.randrange(9999)
+            random_DPC = random.randrange(9999)
+            random_CIC = random.randrange(1000)
+
+
         for m in messages:
             local_messsage = m.copy()
             # Get message timestamp and update it with current time
             m_time = local_messsage['timestamp']
             local_messsage['timestamp'] = current_milli_time()
 
+            if random_option:
+                local_messsage['OPC'] = random_OPC
+                local_messsage['DPC'] = random_DPC
+                local_messsage['CIC'] = random_CIC
+                random_time = random.randrange(0,2)
+
             # Determine how much time we should wait between messages
-            sleep_time = m_time - previous_time
+            sleep_time = m_time - previous_time + random_time
             sleep_time = 0 if sleep_time < 0 else sleep_time # Prevent negativ value on loop
             sleep_time = sleep_time/speed
 
@@ -70,7 +86,7 @@ if __name__ == '__main__':
             # Produce message
             producer.send(topic='simulator', value=json.dumps(local_messsage).encode('UTF-8'))
             logger.info(f'Message {m["MessageType"]} sent')
-            logger.debug(m)
+            logger.debug(local_messsage)
         if repeat:
             logger.info('Repeating ...')
         else:
