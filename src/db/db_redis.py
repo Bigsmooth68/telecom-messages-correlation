ab78616c13e1c6f5fe99fa60851cfd34d440fc81
Bigsmooth68 <olivier.spiesser@gmail.com>
2024-07-30 12:17:46 +0200

    add log to redis

--- src/db/db_redis.py
+++ src/db/db_redis.py
@@ -1,16 +1,33 @@
+# export PYTHONPATH=.
+import sys, os
+sys.path.insert(0, os.getcwd())
+import src.custom_logger.custom_logger as custom_logger
+
 import redis
 
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 def find_xdr(key: str):
-    result = r.get(key)
-    return result
+    try:
+        result = r.get(key)
+        return result
+    except redis.exceptions.ConnectionError:
+        logger.debug('No connection to Redis')
+        exit(1)
 
 def set_xdr(key: str, xdr: str):
-    r.set(key, xdr)
+    try:
+        r.set(key, xdr)
+    except redis.exceptions.ConnectionError:
+        logger.debug('No connection to Redis')
+        exit(1)
 
 def remove_xdr(key: str):
-    r.delete(key)
+    try:
+        r.delete(key)
+    except redis.exceptions.ConnectionError:
+        logger.debug('No connection to Redis')
+        exit(1)
 
 if __name__ == "__main__":
     find_xdr('toto')
